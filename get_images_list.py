02a580250b8cbfe112aff7c2ae45f495fc5a7a42
nicholas hung <nhung93@outlook.com>
2020-05-08 09:41:43 -0400

    trying out creating my own classifier

---
+++ get_images_list.py
@@ -0,0 +1,11 @@
+import glob
+
+def get_image_list(directory, output):
+	images = glob.glob(f"{directory}\\*")
+	f = open(f"{output}.txt", "w")
+
+	for img in images:
+		f.write(img+"\n")
+		
+get_image_list("negative_images", "negative_image_list")
+get_image_list("positive_images", "positive_image_list")
